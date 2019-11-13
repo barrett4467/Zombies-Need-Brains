c1b7c1a50e5a788cbf6c54cb0e1169ff0ed3d072
KailerS <KailerS@users.noreply.github.com>
2019-11-13 11:18:42 -0700

    Added states through hooks for the log in and sign up forms

--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,21 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, FormGroup, Label, Input, Button, FormFeedback, Container } from "reactstrap"
 
-function LogIn () {
+const LogIn = () => {
+    const [user, setUser] = useState({
+        userName: "",
+        password: ""
+    });
+
+    const { userName, password } = user
+
+    const handleInputChangeP = event => {
+        setUser({
+          ...user,
+          password: event.target.value
+        })
+    }
+    const handleInputChangeU = event => {
+        setUser({
+          ...user,
+          userName: event.target.value
+        })
+    }
+
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        alert("Trying to log in!")
+    }
+
+
+
     return (
         <Container style={{ display: "flex", justifyContent: "space-around", textAlign: "center" }}>
             <Form style={{ backgroundColor: "ivory", width: "50%" }}>
                 <h1>Log In!</h1>
                 <FormGroup>
                     <Label for="userName">User Name </Label>
-                    <Input type="userName" name="user" id="userName" placeholder="User Name Here!" />
+                    <Input 
+                        type="userName" 
+                        name="user" 
+                        id="userName" 
+                        placeholder="User Name Here!" 
+                        value={userName}
+                        onChange= {handleInputChangeU} 
+                        />
                 </FormGroup>
                 <FormGroup>
                     <Label for="password">Password </Label>
-                    <Input invalid />
+                    <Input 
+                        type="password" 
+                        name="user" 
+                        id="userName" 
+                        value={password}
+                        onChange={handleInputChangeP}
+                        />
                     <FormFeedback style={{color: "red"}}>Please enter your valid password</FormFeedback>
                 </FormGroup>
-                <Button>Submit</Button>
+                <Button
+                    onClick={handleFormSubmit}
+                >
+                    Submit
+                </Button>
             </Form>
         </Container>
     )
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,27 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, FormGroup, Label, Input, Button, FormFeedback, FormText, Container } from "reactstrap"
 
 function SignUP() {
+
+    const [newUser, setNewUser] = useState({
+        email: "",
+        userName: "",
+        password: ""
+    });
+
+    const { email, userName, password } = newUser
+
+    const handleInputChangeE = event => {
+        setNewUser({
+          ...newUser,
+          email: event.target.value
+        })
+    }
+
+    const handleInputChangeP = event => {
+        setNewUser({
+          ...newUser,
+          password: event.target.value
+        })
+    }
+    const handleInputChangeU = event => {
+        setNewUser({
+          ...newUser,
+          userName: event.target.value
+        })
+    }
+
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        alert("Trying to create account!")
+    }
+
     return (
         <Container style={{ display: "flex", justifyContent: "space-around", textAlign: "center" }}>
             <Form style={{ backgroundColor: "ivory", width: "50%"}}>
                 <h1>Sign UP!</h1>
                 <FormGroup>
                     <Label for="email">Email </Label>
-                    <Input type="email" name="email" id="email" placeholder="123@abc.com" />
+                    <Input 
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        placeholder="123@abc.com"
+                        value={email}
+                        onChange= {handleInputChangeE} 
+                    />
                 </FormGroup>
                 <FormGroup>
                     <Label for="userName">User Name </Label>
-                    <Input valid />
+                    <Input 
+                        type="userName" 
+                        name="user" 
+                        id="userName" 
+                        placeholder="User Name Here!" 
+                        value={userName}
+                        onChange= {handleInputChangeU}
+                     />
                     <FormFeedback valid style={{color:"green"}}>Sweet! that name is available</FormFeedback>
                     <FormText>Enter a unique User Name!</FormText>
                 </FormGroup>
                 <FormGroup>
                     <Label for="password">Password </Label>
-                    <Input invalid />
+                    <Input 
+                        type="password" 
+                        name="user" 
+                        id="userName" 
+                        value={password}
+                        onChange={handleInputChangeP}
+                    />
                     <FormFeedback style={{color: "red"}}>Please Enter a valid password</FormFeedback>
                 </FormGroup>
-                <Button>Submit</Button>
+                <Button
+                    onClick={handleFormSubmit}
+                >Submit</Button>
             </Form>
         </Container>
     )
