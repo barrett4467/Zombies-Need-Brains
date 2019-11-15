f4fb4037257f3035223a47f267822e1300f31bf2
Rebecca Barrett <barrett4467@gmail.com>
2019-11-15 13:00:38 -0700

    Deleted buttons off home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,20 +7,6 @@ function Home () {
         <>   <div style={{display:"flex", justifyContent:"center"}}>
                 <Header></Header>        
             </div>     
-            {/* <div className="buttonHolder" style={{textAlign: "center"}}>
-                <Buttons
-                id="mathButton"
-                destination= "/math"
-                content= "Math!"
-                >
-                </Buttons>
-                <Buttons
-                id="spellingButton"
-                destination="/spelling"
-                content= "Spelling!"
-                >
-                </Buttons>
-            </div> */}
         </>
     )
 }
