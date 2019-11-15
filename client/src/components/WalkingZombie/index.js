84a5da3d424c7d5bc99bae8da8dff4a360f37c11
Rebecca Barrett <barrett4467@gmail.com>
2019-11-15 13:31:42 -0700

    Added gsap to backend

--- client/src/components/WalkingZombie/index.js
+++ client/src/components/WalkingZombie/index.js
@@ -3,7 +3,7 @@ import { TimelineMax } from "gsap/all";
 import Buttons from "../Button/index";
 import CSSPlugin from 'gsap/CSSPlugin';
 
-const C = CSSPlugin; 
+const C = CSSPlugin;
 
 
 //need start stop so that animation will not continue on other pages
@@ -21,53 +21,65 @@ const ZombieComponent = () => {
     let buttonRef = useRef(null);
     let button2Ref = useRef(null);
     let playStopRef = useRef(null);
-    
+
 
     //pulls and starts a new greensock animation tl 
-    const tl = new TimelineMax({ defaults: {duration: 0.25}, paused: true });
+    const tl = new TimelineMax({ defaults: { duration: 0.25 }, paused: true });
     //necessary for react to allow animation
     const animate = (j) => {
         useEffect(() => {
-            tl.set(zombieRef, {attr:{src: "images/"+ j +".png"}});
-            tl.to(zombieRef, {x: x, onComplete: () => x + 5} );
+            tl.set(zombieRef, { attr: { src: "images/" + j + ".png" } });
+            tl.to(zombieRef, { x: x, onComplete: () => x + 5 });
 
         })
     }
     const changeText = () => {
         useEffect(() => {
-            tl.to(welcomeRef, 3, {opacity: 0, onComplete: () => {
-                document.getElementById("story").innerHTML = `<p ref=${element=>{paragraphRef = element}}>Once upon a time, zombies had brains like you and me.</p>`
-            }});
-            tl.fromTo(paragraphRef, 4, {opacity: 0}, {opacity: 100, onComplete: () => {
-                document.getElementById("story").innerHTML = `<p ref=${element=>{secondParagraphRef = element}}>But somewhere along the way, things started to change.</p>`
-            }});
-            tl.fromTo(secondParagraphRef, 4, {opacity: 0}, {opacity: 100, onComplete: () => {
-                document.getElementById("story").innerHTML = `<p ref=${element=>{secondParagraphRef = element}}>There were rumors of a corporation driven by greed who poisoned the air where the zombies,
+            tl.to(welcomeRef, 3, {
+                opacity: 0, onComplete: () => {
+                    document.getElementById("story").innerHTML = `<p ref=${element => { paragraphRef = element }}>Once upon a time, zombies had brains like you and me.</p>`
+                }
+            });
+            tl.fromTo(paragraphRef, 4, { opacity: 0 }, {
+                opacity: 100, onComplete: () => {
+                    document.getElementById("story").innerHTML = `<p ref=${element => { secondParagraphRef = element }}>But somewhere along the way, things started to change.</p>`
+                }
+            });
+            tl.fromTo(secondParagraphRef, 4, { opacity: 0 }, {
+                opacity: 100, onComplete: () => {
+                    document.getElementById("story").innerHTML = `<p ref=${element => { secondParagraphRef = element }}>There were rumors of a corporation driven by greed who poisoned the air where the zombies,
                 who weren’t yet zombies, liked to feed.
                 </p>`
-            }});
-            tl.fromTo(secondParagraphRef, 4, {opacity: 0}, {opacity: 100, onComplete: () => {
-                document.getElementById("story").innerHTML = `<p ref=${element=>{secondParagraphRef = element}}>And slowly but surely the zombies, who weren’t quite zombies, lost their brains.
+                }
+            });
+            tl.fromTo(secondParagraphRef, 4, { opacity: 0 }, {
+                opacity: 100, onComplete: () => {
+                    document.getElementById("story").innerHTML = `<p ref=${element => { secondParagraphRef = element }}>And slowly but surely the zombies, who weren’t quite zombies, lost their brains.
                 </p>`
-            }});
-            tl.fromTo(secondParagraphRef, 4, {opacity: 0}, {opacity: 100, onComplete: () => {
-                document.getElementById("story").innerHTML = `<p ref=${element=>{secondParagraphRef = element}}>It’s now up to you, dear fellow, to turn them back. 
+                }
+            });
+            tl.fromTo(secondParagraphRef, 4, { opacity: 0 }, {
+                opacity: 100, onComplete: () => {
+                    document.getElementById("story").innerHTML = `<p ref=${element => { secondParagraphRef = element }}>It’s now up to you, dear fellow, to turn them back. 
                 </p>`
-            }});
-            tl.fromTo(secondParagraphRef, 4, {opacity: 0}, {opacity: 100, onComplete: () => {
-                document.getElementById("story").innerHTML = `<p ref=${element=>{secondParagraphRef = element}}>Test your skills with math and letter, in hopes of gaining our zombie’s brains back.  
+                }
+            });
+            tl.fromTo(secondParagraphRef, 4, { opacity: 0 }, {
+                opacity: 100, onComplete: () => {
+                    document.getElementById("story").innerHTML = `<p ref=${element => { secondParagraphRef = element }}>Test your skills with math and letter, in hopes of gaining our zombie’s brains back.  
                 </p>`
-            }});
-            tl.fromTo(playStopRef, 5, {opacity: 100}, {opacity: 0});
-            tl.fromTo(buttonRef, 5, {opacity: 0}, {opacity: 100});
-            
+                }
+            });
+            tl.fromTo(playStopRef, 5, { opacity: 100 }, { opacity: 0 });
+            tl.fromTo(buttonRef, 5, { opacity: 0 }, { opacity: 100 });
+
         })
     }
     changeText();
     const callAnimate = () => {
         let i = 0;
         const loop = () => {
-            for (i = 1; i < 11; i++){
+            for (i = 1; i < 11; i++) {
                 animate(i);
             }
         }
@@ -91,29 +103,29 @@ const ZombieComponent = () => {
     return (
         <>
             <div className="wrapper" id="story">
-                <h1 ref={element => {welcomeRef = element}}>
+                <h1 ref={element => { welcomeRef = element }}>
                     Welcome!
                 </h1>
-                <p ref={element => {paragraphRef = element}}></p>
-                <p ref={element=>{secondParagraphRef = element}}></p>
+                <p ref={element => { paragraphRef = element }}></p>
+                <p ref={element => { secondParagraphRef = element }}></p>
 
 
-            </div> 
-            <div ref={element=>{playStopRef = element}}>
+            </div>
+            <div ref={element => { playStopRef = element }}>
                 <button onClick={handleStart}>Play</button>
                 <button onClick={handleStop}>Stop</button>
             </div>
-            <div className="buttonHolder" style={{textAlign: "center"}} ref={element=>{buttonRef = element}}>
+            <div className="buttonHolder" style={{ textAlign: "center" }} ref={element => { buttonRef = element }}>
                 <Buttons
-                id="mathButton"
-                destination= "/math"
-                content= "Math!"
+                    id="mathButton"
+                    destination="/math"
+                    content="Math!"
                 >
                 </Buttons>
                 <Buttons
-                id="spellingButton"
-                destination="/spelling"
-                content= "Spelling!"
+                    id="spellingButton"
+                    destination="/spelling"
+                    content="Spelling!"
                 >
                 </Buttons>
             </div>
