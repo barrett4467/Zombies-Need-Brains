8a9ab4f4d349ba4dba738ec762c8dc4d720ca7dd
Rebecca Barrett <barrett4467@gmail.com>
2019-11-16 15:13:57 -0700

    ?

--- client/src/components/games/Math/MathGame.js
+++ client/src/components/games/Math/MathGame.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as  Router, Route, Switch, Link } from "react-router-dom";
 import mathQuestions from "../../../utils/mathQuestions";
 import styled from "styled-components";
