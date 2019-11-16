a49d1704d48940d56ccf3e523dacaa3d8c6a45e7
Rebecca Barrett <barrett4467@gmail.com>
2019-11-16 15:08:22 -0700

    styling

--- client/src/pages/FirstPage.js
+++ client/src/pages/FirstPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Buttons from "../components/Button"
 import { Container } from "reactstrap";
-import Style from "../utils/style";
 
 function FirstPage () {
     return (
